35e54642261bd0972b14b6c42848d7d2fabe2c80
N-Adeodatus <nkundiman3@gmail.com>
2025-08-24 14:42:12 +0200

    Installed react router and added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { Route, Routes } from "react-router-dom"
 import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
 import Footer from "../components/Footer"
+import Movies from "../components/Movies"
+import About from "../components/About"
+import Favorites from "../components/Favorites"
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <Hero />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
       <Footer />
     </>
   )
