e0de688f2b4494c2ab4c389b887148cfcba4fb07
N-Adeodatus <nkundiman3@gmail.com>
2025-08-24 14:25:08 +0200

    added the About, Favorites, and Movies components

---
+++ components/Favorites.jsx
@@ -0,0 +1,8 @@
+export default function Favorites() {
+    return (
+        <div>
+            <h1>Favorites Page</h1>
+            <p>This is the favorites page of the movie website.</p>
+        </div>
+    );
+}
---
+++ components/Movies.jsx
@@ -0,0 +1,8 @@
+export default function Movies() {
+    return (
+        <div>
+            <h1>Movies Page</h1>
+            <p>This is the movies page of the movie website.</p>
+        </div>
+    );
+}
