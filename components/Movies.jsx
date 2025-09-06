d2e3bef4d52ccf8225f3ffac0d7bcff84372667c
N-Adeodatus <nkundiman3@gmail.com>
2025-09-06 14:03:25 +0200

    added the data from API in the grid elements inside the Movies.jsx

--- components/Movies.jsx
+++ components/Movies.jsx
@@ -1,15 +1,25 @@
 import '../styles/movies.css';
 import { getMovies } from '../fetchMovies';
 import { useSearchParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import star from '../images/yello-star.svg';
+import time from '../images/time.svg';
 export default function Movies() {
     const [searchParams, setSearchParam] = useSearchParams({genre: 'All'})
     const activeGenre = searchParams.get('genre')
     const genres = ['All', 'Action', 'Adventure', 'Comedy', 'Drama', 'Horror', 'Sci-Fi', 'Thriller', 'Romance']
-    async function getMoviesInfo() {
-        const result = await getMovies()
-        console.log(result)
-    }
-    getMoviesInfo()
+    const [moviesInfo, setMoviesInfo] = useState([])
+
+    useEffect(() => {
+        async function getData() {
+            const {results} = await getMovies()
+            setMoviesInfo(results)
+            console.log('movies Info', results)
+        }
+        getData()
+    },[])
+
+
     return (
         <div id='movies-page-container'>
             <h1 id='title'>Trending Movies</h1>
@@ -26,12 +36,35 @@ export default function Movies() {
                     )
                 }
             </div>
+            { moviesInfo.length !== 0 &&
             <div id='cards-container'>
-                <div className="card">
-                    {/* <img src="" alt="moviePoster" /> */}
-
+                {
+                    moviesInfo.map((movie, index) => (
+                        <div className="card">
+                            <div className="HD-badge">
+                                HD
+                            </div>
+                            <div className='image-container'>
+                                <img src={movie.primaryImage} alt="moviePoster" className='moviePoster' />
+                            </div>
+                            <div className="movie-poster-details">
+                                <p className="movie-name">{movie.primaryTitle}</p>
+                                <div className="details-container">
+                                    <div className="duration-container">
+                                        <img src={time} alt="time-icon" className="time-icon" />
+                                        <p className='duration-text'>{movie.runtimeMinutes} min</p>
+                                    </div>
+                                    <div className="rating-container">
+                                        <img src={star} alt="star icon" className='yellow-star-icon' />
+                                        <p className='rating-text'>{movie.averageRating}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
                 </div>
-            </div>
+            }
         </div>
     );
 }
